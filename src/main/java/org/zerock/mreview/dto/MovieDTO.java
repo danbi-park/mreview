e95a930770b40d4fbe2d6c606b6b2190abc0ed10
sweg-db <bhd03067@naver.com>
2021-09-24 14:47:33 +0900

    Changes

--- src/main/java/org/zerock/mreview/dto/MovieDTO.java
+++ src/main/java/org/zerock/mreview/dto/MovieDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.zerock.mreview.entity.MovieImage;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +16,16 @@ import java.util.List;
 @NoArgsConstructor
 public class MovieDTO {
     private Long mno;
+
     private String title;
+
     @Builder.Default
     private List<MovieImageDTO> imageDTOList = new ArrayList<>();
+
+    private  double avg;
+
+    private  int reviewCnt;
+
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
 }
--- src/main/java/org/zerock/mreview/repository/MovieRepository.java
+++ src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -14,11 +14,11 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 //            "left outer join MovieImage mi on mi.movie = m "+
 //            "left outer join Review r on r. movie = m group by m")
     //서브쿼리 사용하여 max적용 가능 -> 조금의 성능 저하
-    @Query("select m, i, count(r) from Movie m left join MovieImage i " +
+/*    @Query("select m, i, count(r) from Movie m left join MovieImage i " +
             "on m=i.movie and i.inum=(select max(i2.inum) from MovieImage i2 " +
             "where i2.movie = m ) " +
             "left outer join Review r on r.movie = m group by m")
-    Page<Object[]> getListPage(Pageable pageable);
+    Page<Object[]> getListPage(Pageable pageable);*/
 
     //평균 평점과 리뷰개수
 /*  @Query("select m, mi from Movie m left outer join MovieImage mi " +
@@ -29,5 +29,10 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
             "left outer join Review r on r.movie = m " +
             "where m.mno =:mno group by mi ")
     List<Object[]> getMovieWithAll(Long mno);
+
+    @Query("select m, mi, avg(coalesce(r.grade,0)), count(r) from Movie m " +
+            "left outer join MovieImage mi on mi.movie = m " +
+            "left outer join Review r on r.movie = m group by m ")
+    Page<Object[]> getListPage(Pageable pageable);
 }
 
